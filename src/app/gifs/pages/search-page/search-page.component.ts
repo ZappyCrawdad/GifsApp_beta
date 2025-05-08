cff0babf1b7cb10b46ccbed06d5160d5017c00d1
ZappyCrawdad <elitovarpalomino@gmail.com>
2025-05-07 23:20:59 -0600

    fix-size-emoji and others pages added to service-ModeDark

--- src/app/gifs/pages/search-page/search-page.component.ts
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,9 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-search-page',
   imports: [],
   templateUrl: './search-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SearchPageComponent { }
+export default class SearchPageComponent implements OnInit, OnDestroy{ 
+
+  constructor(public theme: ThemeService) {}
+  ngOnInit() {
+    this.theme.setPageTheme('theme-home');
+  }
+  ngOnDestroy() {
+    this.theme.clearPageTheme();
+  }
+  toggle() {
+    this.theme.toggleDarkMode();
+  }
+}
--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,9 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-trending-page',
   imports: [],
   templateUrl: './trending-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class TrendingPageComponent { }
+export default class TrendingPageComponent implements OnInit, OnDestroy{ 
+
+  constructor(public theme: ThemeService) {}
+  ngOnInit() {
+    this.theme.setPageTheme('theme-home');
+  }
+  ngOnDestroy() {
+    this.theme.clearPageTheme();
+  }
+  toggle() {
+    this.theme.toggleDarkMode();
+  }
+}
